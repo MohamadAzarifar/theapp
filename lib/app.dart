e3b8d09d1c5fbbd99edb5976b0d31f0f2ae5e140
Mo <azarifar.m.h@outlook.com>
2023-06-23 19:10:30 +0330

    💻 Add the "List Screen" to app's home route.

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:theapp/routes/list_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp();
+  Widget build(BuildContext context) => const MaterialApp(
+        home: ListScreen(),
+      );
 }
