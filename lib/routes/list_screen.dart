67134a6ac70ffca8ea10d9741901de45a9cf7ca5
Mo <azarifar.m.h@outlook.com>
2023-06-23 19:23:54 +0330

    💻 Create the "ScreenUI" widget as Scaffold.

--- lib/routes/list_screen.dart
+++ lib/routes/list_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:theapp/widgets/screen.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Widget build(BuildContext context) => const ScreenUI();
 }
---
+++ lib/widgets/screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ScreenUI extends StatelessWidget {
+  const ScreenUI({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold();
+}
